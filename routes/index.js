187199e849fef162331a5210e2e3a5ce8fd25e1b
WillYip716 <william.yip.716@gmail.com>
2020-07-13 16:49:34 -0700

    api security using jwt

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+var User = require('../models/user');
+const jwt = require('jsonwebtoken')
 
 const api_controller = require('../controllers/apicontroller');
 
@@ -10,18 +12,67 @@ router.get('/posts', api_controller.post_list);
 router.get('/posts/:id', api_controller.single_post);
 
 // Create blog post
-router.post('/posts/create',api_controller.create_post);
+router.post('/posts/create',verifyToken,api_controller.create_post);
 
 // Update blog post
-router.put('/posts/:id',api_controller.update_post);
+router.put('/posts/:id',verifyToken,api_controller.update_post);
 
 // Update blog post to publish
-router.put('/posts/:id/publish', api_controller.publish_post);
+router.put('/posts/:id/publish',verifyToken, api_controller.publish_post);
 
 // Delete post
-router.delete('/posts/:id/delete',api_controller.delete_post);
+router.delete('/posts/:id/delete', verifyToken, api_controller.delete_post);
 
 // create comment in blog post
-router.post('/posts/:id/comment',api_controller.create_comment);
+router.post('/posts/:id/comment', api_controller.create_comment);
+
+router.post("/login", async (req, res) => {
+  console.log("passed name is: " + req.body.username);
+  console.log("passed pw is: " + req.body.password);
+  const user = await User.findOne({ name: req.body.username });
+  if (!user)
+    return res.status(400).send("Username and/or password is incorrect");
+
+  //const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (req.body.password!=user.password) return res.sendStatus(400);
+
+  const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+  res.status(200).send({ token })
+  res.redirect(req.get("referer"));
+
+});
+
+
+
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers['authorization'];
+  // Check if bearer is undefined
+  if(typeof bearerHeader !== 'undefined') {
+    // Split at the space
+    const bearer = bearerHeader.split(' ');
+    // Get token from array
+    const bearerToken = bearer[1];
+    // Set the token
+    try {
+      const verified = jwt.verify(bearerToken, process.env.SECRET);
+      req.user = verified;
+      next();
+    } catch (err) {
+      res.status(400).send("Invalid token");
+    }
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+}
+
+
+
+// GET request for logging out
+router.get("/log-out", (req, res) => {
+  req.logout();
+  res.redirect(req.get("referer"));
+});
 
 module.exports = router;
